d297c1c8b76dc02a8a26f60bbdb4afcd4bea4bd9
Jerhemy Waldon <jswaldon@gmail.com>
2017-11-05 12:52:02 -0700

    Added testing

--- src/app/timeline-smart/timeline-smart.component.spec.ts
+++ src/app/timeline-smart/timeline-smart.component.spec.ts
@@ -1,25 +1,77 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TimelineSmartComponent } from './timeline-smart.component';
-
-describe('TimelineSmartComponent', () => {
-  let component: TimelineSmartComponent;
-  let fixture: ComponentFixture<TimelineSmartComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TimelineSmartComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TimelineSmartComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
+// import {
+//   XHRBackend,
+//   Response,
+//   ResponseOptions, HttpModule
+// } from '@angular/http';
+// import { TimelineSmartComponent } from './timeline-smart.component';
+// import {MockBackend} from "@angular/http/testing";
+// import {TimelineEvent} from "../timeline/timeline-event";
+// import {By} from "@angular/platform-browser";
+//
+// describe('TimelineSmartComponent', () => {
+//   let component: TimelineSmartComponent;
+//   let fixture: ComponentFixture<TimelineSmartComponent>;
+//   let data: TimelineEvent[] = [
+//     {
+//       title: 'Test1',
+//       date: new Date().toLocaleDateString().toString(),
+//       beforeText: 'Text Before Text',
+//       list: ['One', 'Two', 'Three'],
+//       afterText: 'Test After Text'
+//     },
+//     {
+//       title: 'Test2',
+//       date: new Date().toLocaleDateString().toString(),
+//       beforeText: 'Text Before Text',
+//       list: ['One', 'Two', 'Three'],
+//       afterText: 'Test After Text'
+//     },
+//     {
+//       title: 'Test3',
+//       date: new Date().toLocaleDateString().toString(),
+//       beforeText: 'Text Before Text',
+//       list: ['One', 'Two', 'Three'],
+//       afterText: 'Test After Text'
+//     }
+//   ];
+//
+//   beforeEach(async(() => {
+//
+//     TestBed.configureTestingModule({
+//       declarations: [ TimelineSmartComponent ],
+//       imports: [HttpModule],
+//       providers: [
+//         { provide: XHRBackend, useClass: MockBackend }
+//       ]
+//     })
+//
+//     .compileComponents();
+//
+//     fixture = TestBed.createComponent(TimelineSmartComponent);
+//     component = fixture.componentInstance;
+//
+//     fixture.detectChanges();
+//   }));
+//
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+//
+//   it('should create', inject([XHRBackend], (mockBackend) => {
+//       mockBackend.connections.subscribe((connection) => {
+//         connection.mockRespond(new Response(new ResponseOptions({
+//           body: data
+//         })));
+//       });
+//
+//       component.ngOnInit();
+//       let de = fixture.debugElement.query(By.css('h2:first-child'));
+//       let el = de.nativeElement;
+//       const content = el.textContent;
+//       console.log(content);
+//       expect(content).toContain('Test1');
+//
+//     }));
+// });
--- src/app/timeline/timeline.component.spec.ts
+++ src/app/timeline/timeline.component.spec.ts
@@ -1,11 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimelineComponent } from './timeline.component';
+import {TimelineEvent} from "./timeline-event";
+import {By} from "@angular/platform-browser";
 
 describe('TimelineComponent', () => {
   let component: TimelineComponent;
   let fixture: ComponentFixture<TimelineComponent>;
-
+  let el;
+  let data: TimelineEvent[] = [
+    {
+      title: 'Test1',
+      date: new Date().toLocaleDateString().toString(),
+      beforeText: 'Text Before Text',
+      list: ['One', 'Two', 'Three'],
+      afterText: 'Test After Text'
+    },
+    {
+      title: 'Test2',
+      date: new Date().toLocaleDateString().toString(),
+      beforeText: 'Text Before Text',
+      list: ['One', 'Two', 'Three'],
+      afterText: 'Test After Text'
+    },
+    {
+      title: 'Test3',
+      date: new Date().toLocaleDateString().toString(),
+      beforeText: 'Text Before Text',
+      list: ['One', 'Two', 'Three'],
+      afterText: 'Test After Text'
+    }
+  ];
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TimelineComponent ]
@@ -16,10 +41,26 @@ describe('TimelineComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TimelineComponent);
     component = fixture.componentInstance;
+    component.timeline = data;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('first h2 should display correct data', () => {
+    let de = fixture.debugElement.query(By.css('h2:first-child'));
+    let el = de.nativeElement;
+    const content = el.textContent;
+    console.log(content);
+    expect(content).toContain('Test1');
+  });
+
+  it('header element should display correct title', () => {
+    let de = fixture.debugElement.query(By.css('h1.center'));
+    let el = de.nativeElement;
+    const content = el.textContent;
+    expect(content).toContain('Presentation Component');
   });
 });
